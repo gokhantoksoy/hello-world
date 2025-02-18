3b2551c22da8f417d46e5de6f97eb40c2a990ee5
gokhantoksoy <54833412+gokhantoksoy@users.noreply.github.com>
2025-02-18 18:11:09 +0100

    Update and rename Python Learning Notes to Python Learning Notes.py

---
+++ Python Learning Notes.py
@@ -0,0 +1,22 @@
+x = 20.56696
+y = int(input(f"write a number which could be equal to {x} to the power of 2: ")) 
+
+
+match y:
+  case 423: 
+    print ("good job genious")
+  case _:
+    print("stuuupid")
+  
+
+x = 5
+y = int(input(f"write a number which could be equal to {x} to the power of 2: ")) 
+
+
+match y:
+  case y if y > 20 and y < 27 : 
+    print ("easy")
+  case _:
+    print("stuuuuupid")
+
+
